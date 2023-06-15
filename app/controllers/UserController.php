1a079b6bad4df3e566dbf6c226a8f1933552bc68
piouskenny <kehindeu13@gmail.com>
2023-06-15 18:40:55 +0100

    basic PHP project setup

---
+++ app/controllers/UserController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\controllers;
+
+class UserController
+{
+    public function login()
+    {
+        var_dump("This is the Login");
+    }
+
+    public function signup()
+    {
+        var_dump("This is the Login");
+    }
+}
---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'piouskenny/user-auth_api_php',
+        'pretty_version' => '1.0.0+no-version-set',
+        'version' => '1.0.0.0',
+        'reference' => NULL,
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'piouskenny/user-auth_api_php' => array(
+            'pretty_version' => '1.0.0+no-version-set',
+            'version' => '1.0.0.0',
+            'reference' => NULL,
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
