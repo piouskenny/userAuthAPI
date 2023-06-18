ba06197b24dd9412fca29f865deccbeb067fb4b2
piouskenny <kehindeu13@gmail.com>
2023-06-18 21:13:35 +0100

    cleaned up code a bit

--- app/controllers/UserController.php
+++ app/controllers/UserController.php
@@ -4,72 +4,20 @@ namespace App\controllers;
 
 require __DIR__ . "/../../vendor/autoload.php";
 
-use Predis\Client;
 use App\database\Connection;
+
+use App\Services\RateLimitServices;
+use Predis\Client;
 
 class UserController
 {
+    private $RateLimitService;
     private $redis;
-    private $allowedAttempts = 5;
-    private $lockoutDuration = 180; // 3 minutes in seconds
-
-    private $database;
-
-    public function __construct()
-    {
-        $redisOptions = [
-            'scheme' => 'tcp',
-            'host' => '127.0.0.1',
-            'port' => 6379,
-        ];
-        $this->redis = new Client($redisOptions);
-    }
-
-    private function isRateLimitExceeded($username)
-    {
-        $rateLimitKey = 'profile_attempts:' . $username;
-        $attemptCount = (int) $this->redis->get($rateLimitKey);
-
-        if ($attemptCount >= $this->allowedAttempts) {
-            $remainingTime = $this->lockoutDuration - (time() - (int) $this->redis->get('last_failed_profile_time:' . $username));
-            if ($remainingTime > 0) {
-                header("HTTP/1.1 429 Too Many Requests");
-                echo json_encode(['error' => "Too many failed profile attempts. Please try again in $remainingTime seconds."]);
-                return true;
-            } else {
-                $this->redis->del($rateLimitKey);
-                $this->redis->del('last_failed_profile_time:' . $username);
-            }
-        }
-
-        return false;
-    }
-
-    private function incrementRateLimit($username)
-    {
-        $rateLimitKey = 'profile_attempts:' . $username;
-
-        if ($this->redis->exists($rateLimitKey)) {
-            $this->redis->incr($rateLimitKey);
-        } else {
-            $this->redis->set($rateLimitKey, 1, 'EX', $this->lockoutDuration);
-        }
-
-        $this->redis->set('last_failed_profile_time:' . $username, time());
-    }
-
-    private function generateToken($username)
-    {
-        // Generate a custom token here (e.g., using a library like JWT)
-        $token = bin2hex(random_bytes(16));
-
-        $this->redis->set('user_token:' . $username, $token, 'EX', $this->lockoutDuration);
-
-        return $token;
-    }
 
     public function profile()
     {
+        $RateLimitService = new RateLimitServices;
+
         if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             die(json_encode("The GET request is not supported for this route"));
         }
@@ -93,7 +41,7 @@ class UserController
         $storedToken = $this->redis->get('user_token:' . $username);
 
 
-        if ($this->isRateLimitExceeded($username)) {
+        if ($RateLimitService->isRateLimitExceeded($username)) {
             return;
         }
 
@@ -143,6 +91,8 @@ class UserController
 
     public function login()
     {
+        $RateLimitService = new RateLimitServices;
+
         if ($_SERVER['REQUEST_METHOD'] === 'GET') {
             die(json_encode("The GET request is not supported for this route"));
         }
@@ -172,17 +122,17 @@ class UserController
             return;
         }
 
-        if ($this->isRateLimitExceeded($username)) {
+        if ($RateLimitService->isRateLimitExceeded($username)) {
             return;
         }
 
         if ($password !== $user['password']) {
-            $this->incrementRateLimit($username);
+            $RateLimitService->incrementRateLimit($username);
             echo json_encode(['error' => "Invalid username or password"]);
             return;
         }
 
-        $token = $this->generateToken($username);
+        $token = $RateLimitService->generateToken($username);
         $rateLimitKey = 'login_attempts:' . $username;
         $this->redis->del($rateLimitKey);
         $this->redis->del('last_failed_login_time:' . $username);
--- app/services/RateLimitServices.php
+++ app/services/RateLimitServices.php
@@ -2,6 +2,69 @@
 
 namespace App\Services;
 
-class RateLimitServices {
-    
+
+require __DIR__ . "/../../vendor/autoload.php";
+
+use Predis\Client;
+
+class RateLimitServices
+{
+    private $redis;
+    private $allowedAttempts = 5;
+    private $lockoutDuration = 180; // 3 minutes in seconds
+
+    private $database;
+
+    public function __construct()
+    {
+        $redisOptions = [
+            'scheme' => 'tcp',
+            'host' => '127.0.0.1',
+            'port' => 6379,
+        ];
+        $this->redis = new Client($redisOptions);
+    }
+
+    public function isRateLimitExceeded($username)
+    {
+        $rateLimitKey = 'profile_attempts:' . $username;
+        $attemptCount = (int) $this->redis->get($rateLimitKey);
+
+        if ($attemptCount >= $this->allowedAttempts) {
+            $remainingTime = $this->lockoutDuration - (time() - (int) $this->redis->get('last_failed_profile_time:' . $username));
+            if ($remainingTime > 0) {
+                header("HTTP/1.1 429 Too Many Requests");
+                echo json_encode(['error' => "Too many failed profile attempts. Please try again in $remainingTime seconds."]);
+                return true;
+            } else {
+                $this->redis->del($rateLimitKey);
+                $this->redis->del('last_failed_profile_time:' . $username);
+            }
+        }
+
+        return false;
+    }
+
+    public function incrementRateLimit($username)
+    {
+        $rateLimitKey = 'profile_attempts:' . $username;
+
+        if ($this->redis->exists($rateLimitKey)) {
+            $this->redis->incr($rateLimitKey);
+        } else {
+            $this->redis->set($rateLimitKey, 1, 'EX', $this->lockoutDuration);
+        }
+
+        $this->redis->set('last_failed_profile_time:' . $username, time());
+    }
+
+    public function generateToken($username)
+    {
+        // Generate a custom token here (e.g., using a library like JWT)
+        $token = bin2hex(random_bytes(16));
+
+        $this->redis->set('user_token:' . $username, $token, 'EX', $this->lockoutDuration);
+
+        return $token;
+    }
 }
